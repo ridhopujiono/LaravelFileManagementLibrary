3ea2d2c3a0089b4032ece3145722ec3bfb84c935
ridhopujiono <ridho@smktjp.sch.id>
2022-08-29 00:01:33 +0700

    Add 5 static function

--- src/FileManagement.php
+++ src/FileManagement.php
@@ -2,18 +2,90 @@
 
 namespace RidhoPujiono\File;
 
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
+
 /**
  * Basic Calculator.
  *
  */
-class FileManagement {
-	/**
-	 * Menjumlahkan semua data dalam sebuah array.
-	 *
-	 * @param array $data
-	 * @return integer
-	 */
-	public static function say_hello() {
-		return "hello";
-	}
+class FileManagement
+{
+    public static function upload($file, $destination)
+    {
+        if (empty($file)) {
+            return [
+                "status" => "empty",
+                "message" => "File is empty!",
+            ];
+        } else {
+            $nama_file = Storage::putFile(
+                $destination,
+                $file,
+            );
+            return [
+                "status" => "success",
+                "message" => "File is successfully uploaded!",
+                "filename" => basename($nama_file),
+                "folder" => "storage/app/$destination",
+            ];
+        }
+    }
+    public function uploadMultiple($files, $destination)
+    {
+        if (empty($files)) {
+            return [
+                "status" => "empty",
+                "message" => "File is empty!",
+            ];
+        } else {
+            $name_file = [];
+            for ($i = 0; $i < count($files); $i++) {
+                $nama_file[$i] = Storage::putFile(
+                    $destination,
+                    $files[$i],
+                );
+                array_push($name_file, basename($nama_file[$i]));
+            }
+            return [
+                "status" => "success",
+                "message" => "File is successfully uploaded!",
+                "filename" => $name_file,
+                "folder" => "storage/app/$destination",
+            ];
+        }
+    }
+    public function delete($file, $destination)
+    {
+        Storage::delete($destination . "/" . $file);
+        return [
+            "status" => "success",
+            "message" => "File deleted",
+        ];
+    }
+    public function deleteMultiple($files, $destination)
+    {
+        for ($i = 0; $i < count($files); $i++) {
+            Storage::delete($destination . "/" . $files[$i]);
+        }
+        return [
+            "status" => "success",
+            "message" => "Files deleted",
+        ];
+    }
+    public function checkExist($file, $destination)
+    {
+        $check = File::exists(storage_path("app/$destination/$file"));
+        if ($check) {
+            return [
+                "status" => "success",
+                "message" => "File $file is exist!",
+            ];
+        } else {
+            return [
+                "status" => "empty",
+                "message" => "File $file doesnt exist!",
+            ];
+        }
+    }
 }
