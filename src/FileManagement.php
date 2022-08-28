6c8a502ef53ca03dc11e50aa7c193653a7068cdb
ridhopujiono <ridho@smktjp.sch.id>
2022-08-28 23:16:07 +0700

    First Commit

---
+++ src/FileManagement.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace RidhoPujiono\File;
+
+/**
+ * Basic Calculator.
+ *
+ */
+class FileManagement {
+	/**
+	 * Menjumlahkan semua data dalam sebuah array.
+	 *
+	 * @param array $data
+	 * @return integer
+	 */
+	public static function say_hello() {
+		return "hello";
+	}
+}
